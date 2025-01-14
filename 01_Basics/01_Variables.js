110c67f80a3663c5db1b5641519706199ed49773
Ayush Kumar <131509655+ayushkr05@users.noreply.github.com>
2025-01-14 17:19:51 +0000

    Changes in variables

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -3,6 +3,7 @@ let accountName ="Ayush"
 var accountPassword = "usuus"
 accountState = "Bangalore"
 comments = "Hi this is my information"
+let uniqueId;
 
 // accountId = 2 cannot be used as it is constant and cannot be changed.
 
@@ -10,7 +11,9 @@ accountName = "Kumar"
 accountPassword = "jhjghjkg"
 accountState = "Bihar"
 
-console.table([accountId, accountName, accountPassword, accountState, comments]);
+console.log([accountId, accountName, accountPassword, accountState, comments, uniqueId]);
+
+console.table([accountId, accountName, accountPassword, accountState, comments, uniqueId]);
 /*
 Prefer not to use var because of scope as all the var will be changed if we try to change one var in code` 
 */
